8c1f3df2da6579244bb073cf1527369c6129f4c3
Chan Lee <chanethanlee@gmail.com>
2018-08-27 12:38:21 -0700

    add receipient constructor.

--- js/rec-scripts.js
+++ js/rec-scripts.js
@@ -1,3 +1,12 @@
+var receipients = {};
+
+function Receipient(name, age, city, from, to) {
+  this.name = name;
+  this.age =age;
+  this.city = city;
+  this.from = from;
+  this.to = to;
+}
 
 //Front-end ///////////////////////////////////////////////
 $(document).ready(function(){
@@ -5,16 +14,31 @@ $(document).ready(function(){
     event.preventDefault();
 
     var recName = $("#rec-name").val();
-    var recAddress = $("#rec-address").val();
-    var recSupplies = $("#rec-supplies").val();
+    var recAge = $("#rec-age").val();
+    var recCity = $("#rec-city").val();
+    // var recSupplies = $("#rec-supplies").val();
+    // var recLat = parseInt($("#rec-lat").val());
+    // var recLng = parseInt($("#rec-lng").val());
+    var recFrom = $("#rec-from").val();
+    var recTo = $("#rec-to").val();
 
-    console.log(recName);
-    console.log(recAddress);
-    console.log(recSupplies);
+    // console.log(recName);
+    // console.log(recAddress);
+    // console.log(recSupplies);
+    // console.log(recLat);
+    // console.log(recLng);
+    // console.log(recFrom);
+    // console.log(recTo);
+
 
     $("#rec-name-out").text(recName);
-    $("#rec-address-out").text(recAddress);
-    $("#rec-supplies-out").text(recSupplies);
+    $("#rec-age-out").text(recAge);
+    $("#rec-city-out").text(recCity);
+    // $("#rec-supplies-out").text(recSupplies);
+    // $("#rec-lat-out").text(recLat);
+    // $("#rec-lng-out").text(recLng);
+    $("#rec-from-out").text(recFrom);
+    $("#rec-to-out").text(recTo);
 
   });
 });
