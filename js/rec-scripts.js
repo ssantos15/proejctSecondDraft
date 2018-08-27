c5b4a0f6ee3ef0e232d2fa708f93e110552b1226
Chan Lee <chanethanlee@gmail.com>
2018-08-27 13:54:33 -0700

    update city list.

--- js/rec-scripts.js
+++ js/rec-scripts.js
@@ -1,11 +1,13 @@
 var receipients = {};
 
-function Receipient(name, age, city, from, to) {
+function Receipient(name, age, people, city, from, to, message) {
   this.name = name;
   this.age =age;
+  this.people= people;
   this.city = city;
   this.from = from;
   this.to = to;
+  this.message = message;
 }
 
 //Front-end ///////////////////////////////////////////////
@@ -15,12 +17,14 @@ $(document).ready(function(){
 
     var recName = $("#rec-name").val();
     var recAge = $("#rec-age").val();
+    var recPeople = $("#rec-people").val();
     var recCity = $("#rec-city").val();
     // var recSupplies = $("#rec-supplies").val();
     // var recLat = parseInt($("#rec-lat").val());
     // var recLng = parseInt($("#rec-lng").val());
     var recFrom = $("#rec-from").val();
     var recTo = $("#rec-to").val();
+    var recMessage = $("#rec-to").val();
 
     // console.log(recName);
     // console.log(recAddress);
@@ -30,10 +34,13 @@ $(document).ready(function(){
     // console.log(recFrom);
     // console.log(recTo);
 
+    //create Receipient instance based on the input.
+    var recInstance = new Receipient (recName)
 
     $("#rec-name-out").text(recName);
     $("#rec-age-out").text(recAge);
     $("#rec-city-out").text(recCity);
+    $("#rec-people-out").text(recpeople);
     // $("#rec-supplies-out").text(recSupplies);
     // $("#rec-lat-out").text(recLat);
     // $("#rec-lng-out").text(recLng);
